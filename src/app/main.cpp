15dc4502b997526f54ec59bb4dbbfe388aa255f3
Fabio Forni <fabio@redaril.me>
2020-12-21 01:03:47 +0100

    main(): generate a simple PPM image

---
+++ src/app/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int
+main()
+{
+	const auto image_width = 256;
+	const auto image_height = 256;
+
+	const auto max_color = 255;
+
+	// Print PPM header.
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n" << max_color << "\n";
+
+	for (auto y = 0; y < image_height; y++) {
+		for (auto x = 0; x < image_width; x++) {
+			const auto r = static_cast<int>(static_cast<double>(x) / image_width * max_color);
+			const auto g = static_cast<int>(static_cast<double>(y) / image_height * max_color);
+			constexpr auto b = static_cast<int>(0.25 * max_color);
+
+			std::cout << r << ' ' << g << ' ' << b << '\n';
+		}
+	}
+}
