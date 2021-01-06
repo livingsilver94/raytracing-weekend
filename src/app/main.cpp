53cf0fa9744bf77eaaead0bb5f46f3c3fc07ab6a
Fabio Forni <fabio@redaril.me>
2021-01-06 11:53:09 +0100

    Add sphere silhouette

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,7 +4,25 @@
 #include "rtweek/ray.hpp"
 #include "rtweek/vec3.hpp"
 
+bool hit_sphere(const rtweek::Vec3& center, double radius, const rtweek::Ray& r) {
+	auto vec_square = [](const rtweek::Vec3& vec) { return vec.dot(vec); };
+
+	auto oc    = r.origin - center;
+	auto a     = vec_square(r.direction);
+	auto b     = 2.0 * oc.dot(r.direction);
+	auto c     = vec_square(oc) - radius * radius;
+	auto discr = b * b - 4 * a * c;
+	// Note that here we're testing if there's a solution to the equation,
+	// not *where* the solution is in the space, because we are considering
+	// the direction line, not its half-line. This makes possible to see the
+	// sphere even behind the camera.
+	return (discr > 0);
+}
+
 rtweek::Color sky_color(const rtweek::Ray& ray) {
+	if (hit_sphere(rtweek::Vec3(0, 0, -1), 0.5, ray)) {
+		return rtweek::Color(1, 0, 0);
+	}
 	const auto t = 0.5 * (ray.direction.unit().y + 1.0);
 	return (1.0 - t) * rtweek::Color(0.5, 0.7, 1.0) + t * rtweek::Color(1.0, 1.0, 1.0);
 }
