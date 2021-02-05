e4dd4e004995a1590c08258626fbeac4511eb97d
Fabio Forni <fabio@redaril.me>
2021-02-06 00:47:20 +0100

    main(): Add world

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,17 +1,17 @@
 #include <cmath>
 #include <iostream>
 #include <limits>
+#include <memory>
 
 #include "rtweek/color.hpp"
+#include "rtweek/hit.hpp"
 #include "rtweek/ray.hpp"
 #include "rtweek/sphere.hpp"
 #include "rtweek/vec3.hpp"
 
-auto colorize(const rtweek::Ray& ray) -> rtweek::Color {
-	constexpr auto sphere = rtweek::Sphere(rtweek::Vec3(0, 0, -1), 0.5);
-
-	if (auto hit = sphere.hit(ray, 0, std::numeric_limits<double>::infinity()); hit.has_value()) {
-		auto n = hit.value().normal;
+auto colorize(const rtweek::Ray& ray, const rtweek::Hittable& world) -> rtweek::Color {
+	if (auto hit = world.hit(ray, 0, std::numeric_limits<double>::infinity()); hit.has_value()) {
+		const auto n = hit->normal;
 		return 0.5 * rtweek::Color(n.x + 1, n.y + 1, n.z + 1);
 	}
 	const auto t = 0.5 * (ray.direction.unit().y + 1.0);
@@ -24,6 +24,11 @@ int main() {
 	constexpr auto image_width  = 400;
 	constexpr auto image_height = static_cast<int>(image_width / aspect_ratio);
 
+	// World.
+	auto world = rtweek::HittableList();
+	world.add(std::make_shared<rtweek::Sphere>(rtweek::Vec3(0, 0, -1), 0.5));
+	world.add(std::make_shared<rtweek::Sphere>(rtweek::Vec3(0, -100.5, -1), 100));
+
 	// Camera.
 	constexpr auto viewport_height = 2.0;
 	constexpr auto viewport_width  = aspect_ratio * viewport_height;
@@ -35,7 +40,6 @@ int main() {
 
 	// Print PPM header.
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n" << rtweek::max_color << "\n";
-
 	for (auto y = image_height - 1; y >= 0; y--) {
 		std::cerr << "Processing line " << image_height - y << " over " << image_height << '\n';
 		for (auto x = 0; x < image_width; x++) {
@@ -43,7 +47,7 @@ int main() {
 			const auto v = static_cast<double>(y) / (image_height - 1);
 
 			const auto ray   = rtweek::Ray(rtweek::origin, lower_left_corner + u * horizontal + v * vertical);
-			const auto color = colorize(ray);
+			const auto color = colorize(ray, world);
 			std::cout << color << '\n';
 		}
 	}
