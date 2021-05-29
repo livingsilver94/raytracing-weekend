43d416a7036b9a7c54c3d9df60493b8087a53d7a
Fabio Forni <fabio@redaril.me>
2021-05-29 14:09:20 +0200

    main: Save a PPM image with color gradient

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+
+constexpr int MAX_COLOR = 255;
+
 auto main() -> int {
-  return 0;
+  const auto width = 256;
+  const auto height = 256;
+
+  std::cout << "P3\n"
+            << width << ' ' << height << "\n255\n";
+
+  for (auto j = height - 1; j >= 0; j--) {
+    for (auto i = 0; i < width; i++) {
+      const auto r = double(i) / (width - 1);
+      const auto g = double(j) / (height - 1);
+      const auto b = 0.25;
+
+      std::cout << static_cast<int>(MAX_COLOR * r) << ' '
+                << static_cast<int>(MAX_COLOR * g) << ' '
+                << static_cast<int>(MAX_COLOR * b) << '\n';
+    }
+  }
 }
