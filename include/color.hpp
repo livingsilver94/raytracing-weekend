e76ee4a26943ad2627a25c72730ae080af702294
Fabio Forni <fabio@redaril.me>
2021-05-31 23:05:09 +0200

    Color: Implement << operator

--- include/color.hpp
+++ include/color.hpp
@@ -1,7 +1,11 @@
 #ifndef RTWEEK_COLOR_H
 #define RTWEEK_COLOR_H
 
+#include <ostream>
+
 namespace rtweek {
+static constexpr int COLOR_MAX = 255;
+
 class Color {
  public:
   double r = 0;
@@ -21,6 +25,8 @@ class Color {
   auto operator-=(const Color& other) -> Color&;
   auto operator*=(double f) -> Color&;
   auto operator/=(double f) -> Color&;
+
+  friend auto operator<<(std::ostream& out, const Color& c) -> std::ostream&;
 };
 } // namespace rtweek
 
--- src/lib/color.cpp
+++ src/lib/color.cpp
@@ -1,5 +1,7 @@
 #include "color.hpp"
 
+#include <ostream>
+
 namespace rtweek {
 Color::Color() = default;
 Color::Color(double r, double g, double b)
@@ -41,5 +43,12 @@ auto Color::operator/=(double f) -> Color& {
   this->g /= f;
   this->b /= f;
   return *this;
+}
+
+auto operator<<(std::ostream& out, const Color& c) -> std::ostream& {
+  out << static_cast<int>(c.r * COLOR_MAX) << ' '
+      << static_cast<int>(c.g * COLOR_MAX) << ' '
+      << static_cast<int>(c.b * COLOR_MAX) << '\n';
+  return out;
 }
 } // namespace rtweek
