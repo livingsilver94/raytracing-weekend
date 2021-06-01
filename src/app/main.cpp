c469904dbcfe94363d0b7a52346425ca03481d9a
Fabio Forni <fabio@redaril.me>
2021-06-02 00:29:49 +0200

    main: Add red sphere

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,7 +3,19 @@
 #include "color.hpp"
 #include "ray.hpp"
 
+auto hit_sphere(const rtweek::Vec3& center, double radius, const rtweek::Ray& r) -> bool {
+  const auto oc = r.orig - center;
+  const auto a = r.dir.dot(r.dir);
+  const auto b = 2.0 * oc.dot(r.dir);
+  const auto c = oc.dot(oc) - radius * radius;
+  const auto discriminant = b * b - 4 * a * c;
+  return (discriminant > 0);
+}
+
 auto sky(const rtweek::Ray& r) -> rtweek::Color {
+  if (hit_sphere(rtweek::Vec3(0, 0, -1), 0.5, r)) {
+    return rtweek::Color(1.0, 0, 0);
+  }
   const auto t = 0.5 * (r.dir.unit().y + 1.0);
   return (1.0 - t) * rtweek::Color(1.0, 1.0, 1.0) + t * rtweek::Color(0.5, 0.7, 1.0);
 }
