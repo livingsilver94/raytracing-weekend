98ad901acd4ba56e0655c18dac6405764cfaded1
Fabio Forni <fabio@redaril.me>
2022-05-18 22:12:47 +0200

    src/color.rs: Add

---
+++ rust/src/color.rs
@@ -0,0 +1,106 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+
+#[derive(Clone, Copy)]
+pub struct Color {
+	pub r: f64,
+	pub g: f64,
+	pub b: f64,
+}
+
+impl Neg for Color {
+	type Output = Self;
+	fn neg(self) -> Self::Output {
+		Self {
+			r: -self.r,
+			g: -self.g,
+			b: -self.b,
+		}
+	}
+}
+
+impl Add for Color {
+	type Output = Self;
+	fn add(self, other: Self) -> Self::Output {
+		Self {
+			r: self.r + other.r,
+			g: self.g + other.g,
+			b: self.b + other.b,
+		}
+	}
+}
+
+impl Sub for Color {
+	type Output = Self;
+	fn sub(self, other: Self) -> Self::Output {
+		Self {
+			r: self.r - other.r,
+			g: self.g - other.g,
+			b: self.b - other.b,
+		}
+	}
+}
+
+impl Mul for Color {
+	type Output = Self;
+	fn mul(self, other: Self) -> Self::Output {
+		Self {
+			r: self.r * other.r,
+			g: self.g * other.g,
+			b: self.b * other.b,
+		}
+	}
+}
+
+impl Div for Color {
+	type Output = Self;
+	fn div(self, other: Self) -> Self::Output {
+		Self {
+			r: self.r / other.r,
+			g: self.g / other.g,
+			b: self.b / other.b,
+		}
+	}
+}
+
+impl Div<f64> for Color {
+	type Output = Self;
+	fn div(self, f: f64) -> Self::Output {
+		Self {
+			r: self.r / f,
+			g: self.g / f,
+			b: self.b / f,
+		}
+	}
+}
+
+impl AddAssign for Color {
+	fn add_assign(&mut self, other: Self) {
+		self.r += other.r;
+		self.g += other.g;
+		self.b += other.b;
+	}
+}
+
+impl SubAssign for Color {
+	fn sub_assign(&mut self, other: Self) {
+		self.r -= other.r;
+		self.g -= other.g;
+		self.b -= other.b;
+	}
+}
+
+impl MulAssign for Color {
+	fn mul_assign(&mut self, other: Self) {
+		self.r *= other.r;
+		self.g *= other.g;
+		self.b *= other.b;
+	}
+}
+
+impl DivAssign for Color {
+	fn div_assign(&mut self, other: Self) {
+		self.r /= other.r;
+		self.g /= other.g;
+		self.b /= other.b;
+	}
+}
