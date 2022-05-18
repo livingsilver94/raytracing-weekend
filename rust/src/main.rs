9a7f6cd9754d96e2edc157a3995b18d95a9bf2a6
Fabio Forni <fabio@redaril.me>
2022-05-18 21:52:40 +0200

    src/main.rs: Use TAB

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,24 @@
 fn main() {
-    const WIDTH: u32 = 256;
-    const HEIGHT: u32 = 256;
+	const WIDTH: u32 = 256;
+	const HEIGHT: u32 = 256;
 
-    println!("P3");
-    println!("{} {}", WIDTH, HEIGHT);
-    println!("255");
+	println!("P3");
+	println!("{} {}", WIDTH, HEIGHT);
+	println!("255");
 
-    for j in (0..HEIGHT).rev() {
-        eprintln!("\rScanlines remaining: {}", j);
-        for i in 0..WIDTH {
-            let r = i as f64 / (WIDTH - 1) as f64;
-            let g = j as f64 / (WIDTH - 1) as f64;
-            let b = 0.25;
+	for j in (0..HEIGHT).rev() {
+		eprintln!("\rScanlines remaining: {}", j);
+		for i in 0..WIDTH {
+			let r = i as f64 / (WIDTH - 1) as f64;
+			let g = j as f64 / (WIDTH - 1) as f64;
+			let b = 0.25;
 
-            println!(
-                "{} {} {}",
-                (255.0 * r) as i32,
-                (255.0 * g) as i32,
-                (255.0 * b) as i32,
-            );
-        }
-    }
+			println!(
+				"{} {} {}",
+				(255.0 * r) as i32,
+				(255.0 * g) as i32,
+				(255.0 * b) as i32,
+			);
+		}
+	}
 }
