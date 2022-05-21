fc0554ab29f318dc9db4f61cecc7aed3b7bed389
Fabio Forni <fabio@redaril.me>
2022-05-21 22:08:33 +0200

    src/main.rs: Draw the sky

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,43 @@
-use crate::color::Color;
-
 mod color;
+mod ray;
+mod vec3;
+
+use color::Color;
+use ray::Ray;
+use vec3::Vec3;
+
+fn sky_color(ray: &Ray) -> Color {
+	let t = 0.5 * (ray.dir.unit().y + 1.0);
+	Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
+}
 
 fn main() {
-	const WIDTH: u32 = 256;
-	const HEIGHT: u32 = 256;
+	const ASPECT_RATIO: f64 = 16.0 / 9.0;
+	const WIDTH: u32 = 400;
+	const HEIGHT: u32 = WIDTH / ASPECT_RATIO as u32;
+
+	const VIEWPORT_HEIGHT: f64 = 2.0;
+	const VIEWPORT_WIDTH: f64 = VIEWPORT_HEIGHT * ASPECT_RATIO;
+	const FOCAL_LENGTH: f64 = 1.0;
+
+	let horizontal = Vec3 {
+		x: VIEWPORT_WIDTH,
+		y: 0.0,
+		z: 0.0,
+	};
+	let vertical = Vec3 {
+		x: 0.0,
+		y: VIEWPORT_HEIGHT,
+		z: 0.0,
+	};
+	let lower_left_corner = Vec3::origin()
+		- horizontal / 2.0
+		- vertical / 2.0
+		- Vec3 {
+			x: 0.0,
+			y: 0.0,
+			z: FOCAL_LENGTH,
+		};
 
 	println!("P3");
 	println!("{} {}", WIDTH, HEIGHT);
@@ -13,12 +46,13 @@ fn main() {
 	for j in (0..HEIGHT).rev() {
 		eprintln!("\rScanlines remaining: {}", j);
 		for i in 0..WIDTH {
-			let color = Color {
-				r: i as f64 / (WIDTH - 1) as f64,
-				g: j as f64 / (WIDTH - 1) as f64,
-				b: 0.25,
-			}
-			.rgb();
+			let u = i as f64 / (WIDTH - 1) as f64;
+			let v = j as f64 / (HEIGHT - 1) as f64;
+			let ray = Ray {
+				orig: Vec3::origin(),
+				dir: lower_left_corner + horizontal * u + vertical * v,
+			};
+			let color = sky_color(&ray).rgb();
 			println!("{} {} {}", color[0], color[1], color[2]);
 		}
 	}
